ff03569559618ebca25c9349b9d5ea5016e0c693
sakib mahmud galib <sakibmahmudgalib@gmail.com>
2017-12-28 14:32:17 +0600

    up

--- Main/app/view/User/dashboard_view.php
+++ Main/app/view/User/dashboard_view.php
@@ -10,9 +10,10 @@
 				<td>
 					<table cellpadding="25px" width="100%"><tr><td>
 								<table border='1' width="100%" cellpadding="15px">
-						<tr width="100%"><td><a href="myOrders.php"><b>My Orders<b></a><br/>Total Order : <hr/></td></tr>
-						<tr width="100%"><td><a href="myList.php"><b>My WishList<b></a><br/>Total Item : <hr/></td></tr>
-						<trwidth="100%"><td><a href="myReviews.php"><b>My Reviews<b></a><br/>Total Review : <hr/></td></tr>
+						<tr><td><!--a href="myOrders.php"--><b>Orders<b></a><br/>Total Order : <hr/></td></tr>
+						<tr><td><!--a href="myList.php"--><b> WishList<b></a><br/>Total Item : <hr/></td></tr>
+						
+						<tr><td><!--a href="myReviews.php"--><b>Reviews<b></a><br/>Total Review : <hr/></td></tr>
 					</table>
 						</td></tr></table>
 
